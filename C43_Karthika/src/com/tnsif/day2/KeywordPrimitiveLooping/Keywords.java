d4d3bced9a4b8bc2bb2e5142d1302d3142fe5998
ELCOT <ELCOT@Karthika>
2024-08-22 10:08:44 +0530

    commit

--- C43_Karthika/src/com/tnsif/day2/KeywordPrimitiveLooping/Keywords.java
+++ C43_Karthika/src/com/tnsif/day2/KeywordPrimitiveLooping/Keywords.java
@@ -4,11 +4,12 @@ public class Keywords {
 
 	public static void main(String[] args) {
 		int x = 6;
-		if (x != 5) {{{
-			System.out.println("Value of x is not 5");}
+		if (x != 5) {
+			System.out.println("Value of x is not 5");
+			}
 		else {
 		System.out.println("Value of x is 5");
 
 	}
-
+	}
 }

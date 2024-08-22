b0f8e09db5efe5999765fc488011ef2dbaac1038
ELCOT <ELCOT@Karthika>
2024-08-22 11:58:23 +0530

    commit

---
+++ C43_Karthika/src/com/tnsif/day7/Keywords/StaticAbstractFinal/Student.java
@@ -0,0 +1,22 @@
+package com.tnsif.day7.Keywords.StaticAbstractFinal;
+
+abstract class value{
+	void show() {
+		System.out.println("Abstract");
+	}
+}
+class Room extends value{
+     void demo() {
+    	 System.out.println("Print");
+     }
+
+public class Student {
+      //abstract int a; - it is not possible to create a variable using abstract
+	
+	public static void main(String[] args) {
+		Room obj=new Room();
+		obj.show();
+		obj.demo();
+	}
+	}
+}
